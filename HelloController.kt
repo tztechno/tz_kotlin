74ff9eda71031f0dddaf4e83578d215e6d023dfe
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-06-27 01:20:33 +0900

    Add files via upload

---
+++ HelloController.kt
@@ -0,0 +1,13 @@
+package myapp
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello, World!"
+    }
+}
