4289902a1fdd283765f9603b3a455ed29828e4ea
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-12 15:58:29 +0900

    Add files via upload

---
+++ Mkj21Application.kt
@@ -0,0 +1,10 @@
+package mkj21
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class Mkj21Application
+
+fun main(args: Array<String>) {
+    runApplication<Mkj21Application>(*args)
+}
